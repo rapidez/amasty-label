d844af84efb1854d82c33183bf5cb4244e4b688b
Roy Duineveld <royduineveld@gmail.com>
2021-06-16 17:04:42 +0200

    Priority support

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,3 +1,4 @@
-@foreach($product->amasty_label as $label)
+@foreach(collect($product->amasty_label)->sortBy('priority') as $label)
+    @continue($label->is_single && $loop->index)
     <div class="absolute z-10" style="{{ str_replace(["\n", "\r"], '', $label->prod_style) }}">{{ $label->prod_txt }}</div>
 @endforeach
--- src/Models/Scopes/WithProductAmastyLabelScope.php
+++ src/Models/Scopes/WithProductAmastyLabelScope.php
@@ -22,11 +22,17 @@ class WithProductAmastyLabelScope implements Scope
                     "prod_txt", prod.label_text,
                     "prod_style", prod.style,
                     "cat_txt", cat.label_text,
-                    "cat_style", cat.style
+                    "cat_style", cat.style,
+                    "priority", priority,
+                    "is_single", is_single
                 )), "$.null__") as amasty_label')
                 ->leftJoin('amasty_label_index', function ($join) use ($model) {
                     $join->on('amasty_label_index.product_id', '=', $model->getTable() . '.entity_id')
                          ->where('amasty_label_index.store_id', config('rapidez.store'));
+                })
+                ->leftJoin('amasty_label_entity', function ($join) {
+                    $join->on('amasty_label_entity.label_id', '=', 'amasty_label_index.label_id')
+                         ->where('status', 1);
                 })
                 ->leftJoin('amasty_label_catalog_parts as cat', function ($join) {
                     $join->on('cat.label_id', '=', 'amasty_label_index.label_id')
@@ -43,12 +49,17 @@ class WithProductAmastyLabelScope implements Scope
                 "prod_txt", prod_txt,
                 "prod_style", prod_style,
                 "cat_txt", cat_txt,
-                "cat_style", cat_style
+                "cat_style", cat_style,
+                "priority", pos,
+                "is_single", is_single
             )), "$.null__") as amasty_label')
             ->leftJoin('amasty_label_index', function ($join) use ($model) {
                 $join->on('amasty_label_index.product_id', '=', $model->getTable() . '.entity_id')
                      ->where('amasty_label_index.store_id', config('rapidez.store'));
             })
-            ->leftJoin('am_label', 'am_label.label_id', '=', 'amasty_label_index.label_id');
+            ->leftJoin('am_label', function ($join) {
+                $join->on('am_label.label_id', '=', 'amasty_label_index.label_id')
+                     ->where('status', 1);
+            });
     }
 }
