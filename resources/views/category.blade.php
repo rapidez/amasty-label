92014fb3ea8a7399236f1db644b10644b946044c
Robin Mulder <rbn.mulder@gmail.com>
2022-02-05 10:40:49 +0100

    Check if there are labels before sorting (#5)

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,3 +1,3 @@
-<div class="absolute z-10" :style="label.cat_style" v-for="label in Object.values(item.amasty_label).sort((a, b) => a.priority - b.priority).filter((label, index) => !(label.is_single && index))">
+<div class="absolute z-10" :style="label.cat_style" v-for="label in Object.values(item.amasty_label ? item.amasty_label : {}).sort((a, b) => a.priority - b.priority).filter((label, index) => !(label.is_single && index))">
     @{{ label.cat_txt }}
 </div>
