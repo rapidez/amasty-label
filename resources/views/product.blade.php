066aaf1f03508606dc62d6e0ae9738db292227fd
JimmyHoenderdaal <101801301+JimmyHoenderdaal@users.noreply.github.com>
2022-07-25 13:52:28 +0200

    Added position to labels (#9)

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,4 +1,12 @@
 @foreach(collect($product->amasty_label)->sortBy('priority') as $label)
     @continue($label->is_single && $loop->index)
-    <div class="absolute z-10" style="{{ str_replace(["\n", "\r"], '', $label->prod_style) }}">{{ $label->prod_txt }}</div>
+    <div @class([
+        'absolute top-[10px] right-[10px] flex flex-wrap gap-x-[10px]',
+        'top-[10px] left-[10px]' => $label->prod_position === 'top-left',
+        'top-[10px] right-[10px]' => $label->prod_position === 'top-right',
+        'bottom-[10px] left-[10px]' => $label->prod_position === 'bottom-left',
+        'bottom-[10px] right-[10px]' => $label->prod_position === 'bottom-right',
+    ])>
+        <div class="absolute z-10" style="{{ str_replace(["\n", "\r"], '', $label->prod_style) }}">{{ $label->prod_txt }}</div>
+    </div>
 @endforeach
