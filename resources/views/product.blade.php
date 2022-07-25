3d2ce8f7969cefda93b06edc05f854bf3c00e6f8
Roy Duineveld <royduineveld@gmail.com>
2022-07-25 13:51:59 +0200

    Update product.blade.php

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,4 +1,5 @@
 @foreach(collect($product->amasty_label)->sortBy('priority') as $label)
+    @continue($label->is_single && $loop->index)
     <div @class([
         'absolute top-[10px] right-[10px] flex flex-wrap gap-x-[10px]',
         'top-[10px] left-[10px]' => $label->prod_position === 'top-left',
@@ -6,7 +7,6 @@
         'bottom-[10px] left-[10px]' => $label->prod_position === 'bottom-left',
         'bottom-[10px] right-[10px]' => $label->prod_position === 'bottom-right',
     ])>
-        @continue($label->is_single && $loop->index)
         <div class="absolute z-10" style="{{ str_replace(["\n", "\r"], '', $label->prod_style) }}">{{ $label->prod_txt }}</div>
     </div>
 @endforeach
