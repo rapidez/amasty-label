04a7a896b7a4479abc4255694a56d59c87f6faa4
JimmyHoenderdaal <101801301+JimmyHoenderdaal@users.noreply.github.com>
2022-07-25 13:51:17 +0200

    Added position to labels (#10)

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,3 +1,10 @@
 <div class="absolute z-10" :style="label.cat_style" v-for="label in Object.values(item.amasty_label ? item.amasty_label : {}).sort((a, b) => a.priority - b.priority).filter((label, index) => !(label.is_single && index))">
-    @{{ label.cat_txt }}
+    <div class="absolute top-[10px] right-[10px] flex flex-wrap gap-x-[10px]" :class="{
+        'top-[10px] left-[10px]': label.cat_position === 'top-left',
+        'top-[10px] right-[10px]': label.cat_position === 'top-right',
+        'bottom-[10px] left-[10px]': label.cat_position === 'bottom-left',
+        'bottom-[10px] right-[10px]': label.cat_position === 'bottom-right',
+    }">
+        @{{ label.cat_txt }}
+    </div>
 </div>
